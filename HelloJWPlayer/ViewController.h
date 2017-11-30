2697a645e610d506b691c6bc4375357c971a4b96
subhan <dxa04099@gmail.com>
2017-11-30 14:11:58 +0700

    Initial Commit

---
+++ HelloJWPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloJWPlayer
+//
+//  Created by subhan on 11/30/17.
+//  Copyright © 2017 subhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloJWPlayer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HelloJWPlayer
+//
+//  Created by subhan on 11/30/17.
+//  Copyright © 2017 subhan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
